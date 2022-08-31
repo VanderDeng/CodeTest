d4ec7c2027b2e749871faa0e2e4228d4866998cd
vander <vanderdeng@gmail.com>
2022-08-31 11:59:10 +1000

    back-end gradle update

--- front-end/zombie/src/App.js
+++ front-end/zombie/src/App.js
@@ -2,32 +2,40 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Input,
+  Button,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={100}>
+        <Grid minH="20vh" p={100}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <FormControl isRequired>
             <FormLabel>Dimensions of the grid</FormLabel>
             <Input placeholder="Input dimensions of the grid" />
+            <FormLabel>Zombie Initial position</FormLabel>
+            <Input placeholder="Input the initial position of the zombie" />
+            <FormLabel>Creatures Initial position</FormLabel>
+            <Input placeholder="Input a list of initial positions of the creatures" />
+            <FormLabel>Movement List</FormLabel>
+            <Input placeholder="Input a list of moves the zombies will make" />
           </FormControl>
         </Grid>
+        <Button
+          colorScheme="teal"
+          onClick={() => {
+            console.log('!23');
+          }}
+        >
+          Submit
+        </Button>
       </Box>
     </ChakraProvider>
   );
