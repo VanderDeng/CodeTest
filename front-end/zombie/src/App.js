289c1887fcd2d172ffe34ed44b01a2a854a6681e
vander <vanderdeng@gmail.com>
2022-08-31 13:09:07 +1000

    front-end add input var update

--- front-end/zombie/src/App.js
+++ front-end/zombie/src/App.js
@@ -10,8 +10,32 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import axios from 'axios';
 
 function App() {
+  const [allValues, setAllValues] = React.useState({
+    gridSize: '',
+    zombie: '',
+    creatures: '',
+    commands: '',
+  });
+  const handleChange = event =>
+    setAllValues({ ...allValues, [event.target.name]: event.target.value });
+
+  const service = axios.create({
+    baseURL: 'http://localhost:8080',
+    timeout: 1000,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    withCredentials: false,
+  });
+
+  function submitOpt() {
+    service.get('/getPosition').then(res => {
+      console.log(res);
+    });
+    console.log(allValues);
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
@@ -19,21 +43,32 @@ function App() {
           <ColorModeSwitcher justifySelf="flex-end" />
           <FormControl isRequired>
             <FormLabel>Dimensions of the grid</FormLabel>
-            <Input placeholder="Input dimensions of the grid" />
+            <Input
+              name="gridSize"
+              onChange={handleChange}
+              placeholder="Input dimensions of the grid"
+            />
             <FormLabel>Zombie Initial position</FormLabel>
-            <Input placeholder="Input the initial position of the zombie" />
+            <Input
+              name="zombie"
+              onChange={handleChange}
+              placeholder="Input the initial position of the zombie"
+            />
             <FormLabel>Creatures Initial position</FormLabel>
-            <Input placeholder="Input a list of initial positions of the creatures" />
+            <Input
+              name="creatures"
+              onChange={handleChange}
+              placeholder="Input a list of initial positions of the creatures"
+            />
             <FormLabel>Movement List</FormLabel>
-            <Input placeholder="Input a list of moves the zombies will make" />
+            <Input
+              name="commands"
+              onChange={handleChange}
+              placeholder="Input a list of moves the zombies will make"
+            />
           </FormControl>
         </Grid>
-        <Button
-          colorScheme="teal"
-          onClick={() => {
-            console.log('!23');
-          }}
-        >
+        <Button colorScheme="teal" onClick={submitOpt}>
           Submit
         </Button>
       </Box>
