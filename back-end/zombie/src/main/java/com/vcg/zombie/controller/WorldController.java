ff7aa0dc8868a7abee6f817dbc32bd1501b9aa80
Cameron Huang <huangchu103@gmail.com>
2022-09-02 22:56:55 +1000

    No Validations

--- back-end/zombie/src/main/java/com/vcg/zombie/controller/WorldController.java
+++ back-end/zombie/src/main/java/com/vcg/zombie/controller/WorldController.java
@@ -22,72 +22,82 @@ public class WorldController {
     @RequestMapping(value = "/zombie", method = RequestMethod.POST)
     public WorldOutput create(@RequestBody World world) {
 
-        if(checkDigit(world.getGridSize())
-                && checkCommand(world.getCommands())
-                && checkXY(world.getZombie())
-                && checkCreatures(world.getCreatures())
-                && checkDuplicates(world.getZombie(), world.getCreatures()))
-        {
+//        if(checkDigit(world.getGridSize())
+//                && checkCommand(world.getCommands())
+//                && checkXY(world.getZombie())
+//                && checkCreatures(world.getCreatures())
+//                && checkDuplicates(world.getZombie(), world.getCreatures()))
+//        {
             worldService.setWorld(world);
             worldService.getZombies().clear();
             worldService.execute();
             return new WorldOutput(worldService.getZombies(),worldService.getWorld().getCreatures());
-        }
-        else
-        {
-            return null;
-        }
+//        }
+//        else
+//        {
+//            System.out.println(world.getGridSize());
+//            System.out.println(checkDigit(world.getGridSize()));
+//            System.out.println(world.getCommands());
+//            System.out.println(checkCommand(world.getCommands()));
+//            System.out.println(checkXY(world.getZombie()));
+//            System.out.println(checkCreatures(world.getCreatures()));
+//
+//            return null;
+//        }
     }
-
-    public boolean checkCommand(String input)
-    {
-        if (input == null)  // if user did not enter anything
-        {
-            return false;
-        }
-
-        for (int x = 0; x < input.length(); x++) // To go through the typed String
-        {
-            if(((input.charAt(x)) != 'U') || ((input.charAt(x)) != 'D') || ((input.charAt(x)) != 'L') || ((input.charAt(x)) != 'R'))
-            //Call the character method to check each character to see if it is a letter or a space
-            {
-                return false;
-            }
-        }
-
-        return true; // return true if all character in the string is valid
-    }
-
-    public boolean checkDigit(int gridSize)
-    {
-        return Character.isDigit(gridSize);
-    }
-
-    public boolean checkXY(Creature creature)
-    {
-        return checkDigit(creature.getX()) && checkDigit(creature.getY());
-    }
-
-    public boolean checkCreatures(List<Human> creatures)
-    {
-        for (Human creature : creatures) {
-            if (!checkXY(creature)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean checkDuplicates(Zombie zombies, List<Human> creatures)
-    {
-
-        ArrayList<Creature> zombie = new ArrayList<>();
-        zombie.add(zombies);
-        ArrayList<Creature> human = new ArrayList<>(creatures);
-
-        ArrayList<Creature> duplicates = new ArrayList<>(human);
-        duplicates.retainAll(zombie);
-
-        return duplicates.size() == human.size();
-    }
+//
+//    public boolean checkCommand(String input)
+//    {
+//        if (input == null)  // if user did not enter anything
+//        {
+//            return false;
+//        }
+//
+//        for (int x = 0; x < input.length(); x++) // To go through the typed String
+//        {
+//            if(((input.charAt(x)) != 'U') ||
+//                    ((input.charAt(x)) != 'D') ||
+//                    ((input.charAt(x)) != 'L') ||
+//                    ((input.charAt(x)) != 'R'))
+//            //Call the character method to check each character to see if it is a letter or a space
+//            {
+//                return false;
+//            }
+//        }
+//        return true; // return true if all character in the string is valid
+//    }
+//
+//    public boolean checkDigit(int gridSize)
+//    {
+//        int a = 6;
+//        return Character.isDigit(a);
+//    }
+//
+//    public boolean checkXY(Creature creature)
+//    {
+//        return checkDigit(creature.getX()) && checkDigit(creature.getY());
+//    }
+//
+//    public boolean checkCreatures(List<Human> creatures)
+//    {
+//        for (Human creature : creatures) {
+//            if (!checkXY(creature)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    public boolean checkDuplicates(Zombie zombies, List<Human> creatures)
+//    {
+//
+//        ArrayList<Creature> zombie = new ArrayList<>();
+//        zombie.add(zombies);
+//        ArrayList<Creature> human = new ArrayList<>(creatures);
+//
+//        ArrayList<Creature> duplicates = new ArrayList<>(human);
+//        duplicates.retainAll(zombie);
+//
+//        return duplicates.size() == human.size();
+//    }
 }
