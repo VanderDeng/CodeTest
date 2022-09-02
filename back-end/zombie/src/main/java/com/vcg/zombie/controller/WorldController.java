0eb2da8985023026e14ac03cee83e3a9baff20ae
GordonWSL <sgz886@gmail.com>
2022-09-02 14:07:39 +1000

    fix: fixed zombie duplicate bug

--- back-end/zombie/src/main/java/com/vcg/zombie/controller/WorldController.java
+++ back-end/zombie/src/main/java/com/vcg/zombie/controller/WorldController.java
@@ -34,6 +34,7 @@ public class WorldController {
     @RequestMapping(value = "/zombie", method = RequestMethod.POST)
     public WorldOutput create(@RequestBody World world) {
         worldService.setWorld(world);
+        worldService.getZombies().clear();
         worldService.execute();
         return new WorldOutput(worldService.getZombies(),worldService.getWorld().getCreatures());
     }
