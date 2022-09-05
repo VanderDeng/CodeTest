18d00eec462416a01a08f541d0839002edf0c557
GordonWSL <sgz886@gmail.com>
2022-09-05 22:40:54 +1000

    refine: Zombie and Creature contains Position field variable

--- back-end/zombie/src/main/java/com/vcg/zombie/entity/Zombie.java
+++ back-end/zombie/src/main/java/com/vcg/zombie/entity/Zombie.java
@@ -1,10 +1,27 @@
 package com.vcg.zombie.entity;
 
-public class Zombie extends Position {
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.vcg.zombie.service.WorldService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
 
-    public Zombie(int x, int y) {
-        this.x = x;
-        this.y = y;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Zombie {
+    Position position = new Position();
+
+    @JsonAnySetter
+    public void getXYFromJson(String key, String value) {
+        if ("x".equals(key)) {
+            position.setX(Integer.parseInt(value));
+        } else if ("y".equals(key)) {
+            position.setY(Integer.parseInt(value));
+        } else {
+            LogManager.getLogger(Zombie.class).info("met abnormal value: " + value);
+        }
     }
 
 }
